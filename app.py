55030a5d01ce801b97bf3df97d2d1deaeb547302
nguyenquochuy17 <quochuynguyen2810@gmail.com>
2025-06-25 23:32:32 +0700

    update

--- app.py
+++ app.py
@@ -12,19 +12,29 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-YOUR_USER_ID = 688659963052425357 # Replace with your Discord user ID
-ENTRANCE_AUDIO = "join_sound.mp3"
+# Map of user ID to their entrance audio file
+USER_ENTRANCE_AUDIO = {
+    688659963052425357: "join_sound.mp3",  # You
+    688386424612847618: "ht2.mp3"          # Your friend
+}
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    if member.id == YOUR_USER_ID and after.channel is not None:
-        vc = await after.channel.connect()
-        vc.play(discord.FFmpegPCMAudio(ENTRANCE_AUDIO))
+    # Only act if the user is in our dictionary and they joined/moved voice channels
+    if member.id in USER_ENTRANCE_AUDIO and before.channel != after.channel and after.channel is not None:
+        audio_file = USER_ENTRANCE_AUDIO[member.id]
+
+        try:
+            vc = await after.channel.connect()
+        except discord.ClientException:
+            # Already connected to a channel
+            return
+
+        vc.play(discord.FFmpegPCMAudio(audio_file))
 
         while vc.is_playing():
             await asyncio.sleep(1)
 
         await vc.disconnect()
-
 
 bot.run(os.getenv("DISCORD_BOT_TOKEN"))
